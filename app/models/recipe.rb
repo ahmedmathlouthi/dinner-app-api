7a70635619125e58f5f7ceea314eaaed1acf8983
Ahmed Mathlouthi <ahmed.mathlouthi.am@gmail.com>
2022-02-24 00:37:59 +0100

    fix: matching score method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,18 +2,17 @@ class Recipe < ApplicationRecord
   PER_PAGE = 10
 
   # TODO: in case we want to get the exact matching score to get higher precision
+  def matching_score(keys)
+    score = 0
+    ingredients.each do |ingredient|
+      score += 1 if ingredient_contains_key(ingredient, keys)
+    end
+    ((score.to_f / ingredients.length)).to_i
+  end
 
-  # def matching_score(keys)
-  #   score = 0
-  #   ingredients.each do |ingredient|
-  #     score += 1 if ingredient_contains_key(ingredient, keys)
-  #   end
-  #   ((score.to_f / ingredients.length)).to_i
-  # end
+  private
 
-  # private
-
-  # def ingredient_contains_key(ingredient, keys)
-  #   keys.any? { |key| key.in?(ingredient) }
-  # end
+  def ingredient_contains_key(ingredient, keys)
+    keys.any? { |key| key.in?(ingredient) }
+  end
 end
